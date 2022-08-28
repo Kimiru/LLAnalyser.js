a1ff32f15388f08e60b594f206aff824c76bf96f
Laurent SIMONET <laursim24@gmail.com>
2022-08-28 10:02:10 +0200

    clean exemple

--- example/mathexpr/example.js
+++ example/mathexpr/example.js
@@ -1,10 +1,6 @@
 const { mathexpr_lla, calculate } = require('./mathexpr')
 
-console.log(mathexpr_lla.follow('Ep'))
-
 let ast = mathexpr_lla.parse('( 1+-2 * (1/2) --1) * 1e2')
-
-console.log(ast)
 
 
 console.log(calculate(ast.flatten()))
